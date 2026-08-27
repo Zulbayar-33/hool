561360d6f21236ecd74f7c349adbf8a2e019ba4d
Zulbayar-33 <114511555+Zulbayar-33@users.noreply.github.com>
2026-08-27 12:48:09 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbxH_0OxC_qNg59DfDtZwcO8WjA7nvPjIJQ1x3u1BM64ltwodjWeGVdvp6DSONJ8rMftvA/exec",
+  TIMEZONE: "Asia/Ulaanbaatar"
+});
