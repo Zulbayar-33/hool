9144adaf00e1efe0fd9b19e6460e0fbbf7d29f53
Zulbayar-33 <114511555+Zulbayar-33@users.noreply.github.com>
2026-08-27 14:44:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
-  API_URL: "PASTE_YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbyFfyhT0b6VL6Py-7MS-IEb3Zq__NiDY3WOLwNcOtvVje-IQiO3PL4XYpAAR1TMiPc5cQ/exec",
   TIMEZONE: "Asia/Ulaanbaatar"
 });
