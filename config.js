f15dd907718d2dc7df6035851e1f30809416e14b
Zulbayar-33 <114511555+Zulbayar-33@users.noreply.github.com>
2026-08-27 14:39:50 +0800

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycbxH_0OxC_qNg59DfDtZwcO8WjA7nvPjIJQ1x3u1BM64ltwodjWeGVdvp6DSONJ8rMftvA/exec",
-  TIMEZONE: "Asia/Ulaanbaatar"
-});
