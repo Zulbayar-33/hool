e5d0a3a76a60934da148c9b4824234fda78210bb
Zulbayar-33 <114511555+Zulbayar-33@users.noreply.github.com>
2026-09-11 14:03:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycbx0obQovsqGFOXOeUO2gj7UMSSfu5m6vPLGZdjp2pgRYscdwdIbfUIW4dXpcMIAu6RTBw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxC7EcSNAtPLyfRm534vBYXh4BNN5DQ3LPJoa0Z66iDKDjrRymUu5t_G__crZrlSDj02A/exec",
   TIMEZONE: "Asia/Ulaanbaatar"
 });
